95ca046cebbf7d6406bbc657274910204fe028e6
AsD Monio <asdmonio@gmail.com>
2019-02-05 16:32:25 +0100

    montana: Don't use the common sensor config
    Change-Id: I974005c7034124d73c3f76580be447e600664029

--- device.mk
+++ device.mk
@@ -53,7 +53,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:system/vendor/etc/sensors/hals.conf \
-    $(LOCAL_PATH)/../msm8953-common/configs/sensors/sensor_def_qcomdev.conf:system/vendor/etc/sensors/sensor_def_qcomdev.conf
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/vendor/etc/sensors/sensor_def_qcomdev.conf
 
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service_32 \
